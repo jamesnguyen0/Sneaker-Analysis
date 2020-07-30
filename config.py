54f7e68914953c66df944331c6eb40454a906b53
James Nguyen <jamesnguyen0@gmail.com>
2020-07-29 22:24:34 -0700

    reupload again

---
+++ config.py
@@ -0,0 +1,9 @@
+twitter_apikey = "MD1lpQHWFTnp2hAvTz8XI1usn"
+
+twitter_secretkey = "wEEeWFAQEEhQxgaVTRopaQjhmsYvJwu6KhbjSUBUq1kmk1uRDs"
+    
+twitter_token = "AAAAAAAAAAAAAAAAAAAAAHixGAEAAAAAGuFpNqpK8lLxhUxEbBl9%2Bzc6Yfw%3D49x5SVh6PkDCxuZMVzcrV3AJeWsgJZkNN6vAXOlA5qT2x3nNva"
+
+gkey="AIzaSyBD0e-7oE5jYCTUtZUVOZrMStUEW0K7G9A"
+
+census_key = "4c5d7452b380ff2a0ec6f459ecde5891b1b03d27"
